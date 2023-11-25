76c9fa5372dce7f9b5e9c1ee888faff78d0a39f2
pveom <pveomegq@gmail.com>
2023-11-25 16:23:04 +0200

    Add project files.

---
+++ dz1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+dz1.Rectangles figure = new dz1.Rectangles(10, 5);
+
+Console.WriteLine("Перша сторона " + figure.side1);
+Console.WriteLine("Друга сторона " + figure.side2);
+
+Console.WriteLine("Площа прямокутника = " + figure.Area);
+Console.WriteLine("Периметр прямокутника = " + figure.Perimeter);
+
+Console.ReadLine();
---
+++ dz1/Rectangles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz1
+{
+    internal class Rectangles
+    {
+
+        public double side1;
+        public double side2;
+        public double Side1 { get; }
+        public double Side2 { get; }
+        public double Area { get; }
+
+        public double Perimeter { get; }
+
+        public Rectangles(double side1, double side2)
+
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.Area = AreaCalculator(side1, side2);
+            this.Perimeter = PerimeterCalculator(side2, side1);
+
+            
+
+        }
+
+       
+
+        #region
+        public double AreaCalculator(double side1, double side2)
+        {
+            return Math.Abs(side1 * side2);
+        }
+        public double PerimeterCalculator(double side1, double side2)
+        {
+            return Math.Abs(side1 * 2) + (side2 * 2);
+        }
+        #endregion
+
+    }
+}
