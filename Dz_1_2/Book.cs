4e14bfa126ba4154c1f42ae6cd5085fc088ea6f3
pveom <pveomegq@gmail.com>
2023-11-25 17:30:35 +0200

    dz1-2

---
+++ Dz_1_2/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_1_2
+{
+    internal class Book
+    {
+
+        string Title;
+
+        string Author;
+
+
+        string Content;
+        
+        public Book(string title , string author, string content)
+        { 
+            this.Title = title;
+            this.Author = author;
+            this.Content = content;
+        
+        }
+        public void Show()
+        { 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Назва книги " + Title);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Автор " + Author);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Контекст " + Content);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ Dz_1_2/Program.cs
@@ -0,0 +1,6 @@
+﻿using Dz_1_2;
+
+Book book1 = new Book("Findd","Grog","bibi baba gogo ");
+book1.Show(); 
+Book book2 = new Book("Gorila","Mah","Gugu gaga lalala");
+book2.Show();
