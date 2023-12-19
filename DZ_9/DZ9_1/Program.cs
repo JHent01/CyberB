45b28f835b40a4e67e13d30b44b6282b3069016f
pveom <pveomegq@gmail.com>
2023-12-19 16:41:13 +0200

    dz9

---
+++ DZ_9/DZ9_1/Program.cs
@@ -0,0 +1,38 @@
+﻿  int Parse(string a)
+{
+    bool tryParse;
+    int parse;
+    tryParse = int.TryParse(a, out parse);
+    if (tryParse == true)
+    {
+        parse = int.Parse(a);
+        return parse;
+    }
+    else Console.WriteLine("Введено не число");
+    return 0;
+}
+int a,b,c;
+string pars;
+Console.WriteLine("Введіть число 1");
+pars=Console.ReadLine();
+a = Parse(pars);
+Console.WriteLine("Введіть число 2");
+pars = Console.ReadLine();
+b = Parse(pars);
+c = 0;
+
+Func<int,int> Add = c=> a+b;
+Console.WriteLine(Add(c));
+Func<int,int> Sub = c=> a-b;
+Console.WriteLine(Sub(c));
+Func<int,int> Mul = c=> a*b;
+Console.WriteLine(Mul(c));
+try
+{
+    Func<int, int> Div = c => a / b;
+    Console.WriteLine(Div(c));
+}
+catch (Exception ex)
+{ Console.WriteLine(ex.Message); }
+
+
---
+++ DZ_9/DZ9_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+class Program
+{
+    delegate int Number();
+    delegate double MyDel(Number[] a);
+
+    
+
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+         int GetRandom()
+         {
+
+            int number = random.Next(10);
+            Console.WriteLine(number);
+            return number;
+         }
+       
+        
+
+        Console.WriteLine("Введіть кількість елементів масива: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(new string('-', 50));
+
+        var array = new Number[n];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = () => new Number(GetRandom)();
+        }
+
+        MyDel ser = delegate (Number[] num)
+        {
+            double sr = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                sr += num[i]();
+            }
+            return sr / array.Length;
+        };
+
+        Console.WriteLine($"Среднее ариф = {ser(array)}");
+       
+
+        
+    }
+}
